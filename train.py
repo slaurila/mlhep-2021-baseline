103b8754849da73e4bccd22d9471bcbf8027d1ba
Nikita Kazeev <kazeevn@gmail.com>
2021-07-27 16:13:19 +0300

    Automatic free GPU selection

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import configparser
 import pathlib as path
 
+import numpy as np
 import pytorch_lightning as pl
 from pytorch_lightning import seed_everything
 
@@ -8,14 +9,32 @@ from idao.data_module import IDAODataModule
 from idao.model import SimpleConv
 
 
+def get_free_gpu():
+    """
+    Returns the index of the GPU with the most free memory.
+    Different from lightning's auto_select_gpus, as it provides the most free GPU, not an absolutely free.
+    """
+    from pynvml import nvmlInit, nvmlDeviceGetHandleByIndex, nvmlDeviceGetMemoryInfo, nvmlDeviceGetCount
+    nvmlInit()
+
+    return np.argmax([
+        nvmlDeviceGetMemoryInfo(nvmlDeviceGetHandleByIndex(i)).free
+        for i in range(nvmlDeviceGetCount())
+    ])
+
+
 def trainer(mode: ["classification", "regression"], cfg, dataset_dm):
     model = SimpleConv(mode=mode)
+    if cfg.getboolean("TRAINING", "UseGPU"):
+        gpus = [get_free_gpu()]
+    else:
+        gpus = None
     if mode == "classification":
         epochs = cfg["TRAINING"]["ClassificationEpochs"]
     else:
         epochs = cfg["TRAINING"]["RegressionEpochs"]
     trainer = pl.Trainer(
-        gpus=int(cfg["TRAINING"]["NumGPUs"]),
+        gpus=gpus,
         max_epochs=int(epochs),
         progress_bar_refresh_rate=20,
         weights_save_path=path.Path(cfg["TRAINING"]["ModelParamsSavePath"]).joinpath(
